c1283209cb89471c1c596a4ea7aed4f1352c3a91
buluma <michael@buluma.me.ke>
2018-02-07 20:41:26 +0300

    Joomla 3.8.5 update

---
+++ administrator/logs/joomla_update.php
@@ -0,0 +1,14 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2018-02-07 17:31:11 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2018-02-07T17:31:11+00:00	INFO ::1	update	Update started by user Super User (542). Old version is 3.8.4.
+2018-02-07T17:31:19+00:00	INFO ::1	update	Downloading update file from https://s3-us-west-2.amazonaws.com/joomla-official-downloads/joomladownloads/joomla3/Joomla_3.8.5-Stable-Update_Package.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIZ6S3Q3YQHG57ZRA%2F20180207%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20180207T173126Z&X-Amz-Expires=60&X-Amz-SignedHeaders=host&X-Amz-Signature=6815acc47fd787dc606160d9bb028dd56341c5a3cb3f89f24272f8f57c2a08dd.
+2018-02-07T17:31:30+00:00	INFO ::1	update	File Joomla_3.8.5-Stable-Update_Package.zip downloaded.
+2018-02-07T17:31:31+00:00	INFO ::1	update	Starting installation of new version.
+2018-02-07T17:32:22+00:00	INFO ::1	update	Finalising installation.
+2018-02-07T17:32:23+00:00	INFO ::1	update	Deleting removed files and folders.
+2018-02-07T17:32:27+00:00	INFO ::1	update	Cleaning up after installation.
+2018-02-07T17:32:27+00:00	INFO ::1	update	Update to version 3.8.5 is complete.
