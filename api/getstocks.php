66b3bdf8978637130749c29275fb6da448dba96e
buluma <michael@buluma.me.ke>
2018-03-21 20:32:33 +0300

    stocks

--- api/getstocks.php
+++ api/getstocks.php
@@ -1,14 +1,17 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Authorization, Origin, X-Requested-With, Content-Type, Accept");
 error_reporting(1);
-require_once '../ewcfg11.php';
+require_once 'connection.php';
 
-$mysqli = mysqli_connect(EW_CONN_HOST,EW_CONN_USER,EW_CONN_PASS,EW_CONN_DB) or die("Error " . mysqli_error($mysqli));
+// $mysqli = mysqli_connect(EW_CONN_HOST,EW_CONN_USER,EW_CONN_PASS,EW_CONN_DB) or die("Error " . mysqli_error($mysqli));
+$mysqli = mysqli_connect($host,$user,$password,$db) or die("Error " . mysqli_error($mysqli));
 
 if (isset($_GET['store_server_id'])){
    	$json = array();
    	$id = $_GET['store_server_id'];
 
-   	$query = "SELECT * FROM data_brandstocks WHERE store_server_id = '".$id."'";
+   	$query = "SELECT * FROM dxcr2_brandstocks WHERE store_server_id = '".$id."'";
    	$result = mysqli_query($mysqli,$query) or die(mysqli_error($mysqli));
    	$total = mysqli_num_rows($result);
    
