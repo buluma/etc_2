9364f416f0aa5e7936105390a445bcc18920c4cf
buluma <michael@buluma.me.ke>
2018-04-09 09:04:35 +0300

    test

--- administrator/logs/error.php
+++ administrator/logs/error.php
@@ -5,3 +5,4 @@
 
 #Fields: datetime	priority clientip	category	message
 2018-03-03T15:51:01+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2018-04-06T20:27:35+00:00	INFO ::1	joomlafailure	Empty password not allowed.
