f1288401bb6936d9b1db33d0699863c277e13f19
buluma <michael@buluma.me.ke>
2018-04-21 04:59:06 +0300

    bts export

--- administrator/components/com_bts_items/views/bts_items/tmpl/default.php
+++ administrator/components/com_bts_items/views/bts_items/tmpl/default.php
@@ -20,6 +20,20 @@ $document = JFactory::getDocument();
 $document->addStyleSheet(JUri::root() . 'administrator/components/com_bts_items/assets/css/bts_items.css');
 $document->addStyleSheet(JUri::root() . 'media/com_bts_items/css/list.css');
 
+//datatables
+$document->addStyleSheet('//cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css');
+$document->addStyleSheet('//cdn.datatables.net/buttons/1.5.1/css/buttons.dataTables.min.css');
+$document->addScript('//cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js');
+
+//export pdf
+$document->addScript('//cdn.datatables.net/buttons/1.5.1/js/dataTables.buttons.min.js');
+$document->addScript('//cdn.datatables.net/buttons/1.5.1/js/buttons.flash.min.js');
+$document->addScript('//cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js');
+$document->addScript('//cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/pdfmake.min.js');
+$document->addScript('//cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/vfs_fonts.js');
+$document->addScript('//cdn.datatables.net/buttons/1.5.1/js/buttons.html5.min.js');
+$document->addScript('//cdn.datatables.net/buttons/1.5.1/js/buttons.print.min.js');
+
 $user      = JFactory::getUser();
 $userId    = $user->get('id');
 $listOrder = $this->state->get('list.ordering');
@@ -95,9 +109,6 @@ $sortFields = $this->getSortFields();
 				<th class='left'>
 				<?php echo JHtml::_('searchtools.sort',  'COM_BTS_ITEMS_BTS_ITEMS_COORDINATES', 'a.`coordinates`', $listDirn, $listOrder); ?>
 				</th>
-				<!-- <th class='left'>
-				<?php //echo JHtml::_('searchtools.sort',  'COM_BTS_ITEMS_BTS_ITEMS_CREATED', 'a.`created`', $listDirn, $listOrder); ?>
-				</th> -->
 				<th class='left'>
 				<?php echo JHtml::_('searchtools.sort',  'COM_BTS_ITEMS_BTS_ITEMS_CLIENT_ID', 'a.`client_id`', $listDirn, $listOrder); ?>
 				</th>
@@ -190,11 +201,8 @@ $sortFields = $this->getSortFields();
 				</td>	
 				<td>
 					<?php echo $item->coordinates; ?>
-				</td>					
-				<td class="hidden">
-					<?php echo $item->created; ?>
-				</td>				<td>
-
+				</td>						
+				<td>
 					<?php echo $item->client_id; ?>
 				</td>
 
@@ -210,6 +218,16 @@ $sortFields = $this->getSortFields();
 		</div>
 </form>
 <script>
+	//export function
+	jQuery(document).ready(function() {
+	    jQuery('#bts_itemList').DataTable( {
+	        dom: 'Bfrtip',
+	        buttons: [
+	            'copy', 'csv', 'excel', 'pdf', 'print'
+	        ]
+	    } );
+	} );
+
     window.toggleField = function (id, task, field) {
 
         var f = document.adminForm, i = 0, cbx, cb = f[ id ];
--- administrator/logs/error.php
+++ administrator/logs/error.php
@@ -9,3 +9,7 @@
 2018-04-17T00:03:00+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
 2018-04-17T09:42:27+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
 2018-04-18T14:14:22+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2018-04-20T23:56:46+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2018-04-20T23:56:54+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2018-04-21T00:10:49+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2018-04-21T00:25:51+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
